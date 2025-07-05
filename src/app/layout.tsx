737befb4c404286331d840ae8d38919af955dce3
GOURAB SAHA <gourabhere@gmail.com>
2025-07-05 20:06:04 +0000

    Update1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: ' ',
-  description: ' ',
+  title: 'Hijibiji Announcement',
+  description: 'Hijibiji Maintenance and Security Announcement',
 };
 
 export default function RootLayout({
